e8a42b3c986776ac9f0dd51da025ee9650524a2a
René Donaire <rene.donaire@gmail.com>
2021-06-05 13:32:16 -0400

    Update scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,13 +1,32 @@
-// Pide datos al usuario
+/* -------------------------------------------------------------------------- */
+/*                            Pide datos al usuario                           */
+/* -------------------------------------------------------------------------- */
 let nombre = prompt("¿Cómo es tu nombre?");
-let apellido = prompt("¿Y tu apellido?");
-let añoNacimiento = prompt("¿En qué año naciste?");
+if (nombre == "") {
+    alert("Dejaste tu nombre vacío.")
+    // ¿Cómo vuelvo a pedir el nombre?
+}
 
-// Averigua el año actual - gracias, san Google
+let apellido = prompt("¿Y tu apellido?");
+if (apellido == "") {
+    alert("No me diste tu apellido.")
+    //¿Cómo vuelvo a pedir el apellido?
+}
+
+let añoNacimiento = prompt("¿En qué año naciste?");
+// ¿Cómo verifico que sea un año válido?
+
+
+/* -------------------------------------------------------------------------- */
+/*                Averigua el año actual - gracias, san Google                */
+/* -------------------------------------------------------------------------- */
 var fecha = new Date();
 var añoActual = fecha.getFullYear();
 
-// Salida
+
+/* -------------------------------------------------------------------------- */
+/*                                   Salida                                   */
+/* -------------------------------------------------------------------------- */
 let edad = parseInt(añoActual - añoNacimiento);
 let salida = "Bueno " + nombre + " " + apellido + ", tienes " + edad + " años.";
 alert(salida);
