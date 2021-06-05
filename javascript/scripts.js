7610cbd697a8326a811315a0232a7919e12b1b1a
René Donaire <rene.donaire@gmail.com>
2021-06-05 12:34:43 -0400

    Operaciones básicas en javascript

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,1 +1,13 @@
-// 
+// Pide datos al usuario
+let nombre = prompt("¿Cómo es tu nombre?");
+let apellido = prompt("¿Y tu apellido?");
+let añoNacimiento = prompt("¿En qué año naciste?");
+
+// Averigua el año actual - gracias, san Google
+var fecha = new Date();
+var añoActual = fecha.getFullYear();
+
+// Salida
+let edad = parseInt(añoActual - añoNacimiento);
+let salida = "Bueno " + nombre + " " + apellido + ", tienes " + edad + " años.";
+alert(salida);
