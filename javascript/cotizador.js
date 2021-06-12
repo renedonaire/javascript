116d7f2373c228e89ce35600030d9e2f60a64c9c
René Donaire <rene.donaire@gmail.com>
2021-06-12 02:17:43 -0400

    Corrige errores en funciones

--- javascript/cotizador.js
+++ javascript/cotizador.js
@@ -18,57 +18,58 @@ let precio = 0;
 /*                               Define funciones                             */
 /* -------------------------------------------------------------------------- */
 // Pide el nombre del usuario y repite mientras no se ingrese un texto
-function pideNombre(nombre) {
+function pideNombre() {
     nombre = prompt("¿Cual es su nombre?");
-    while (nombre === "" || parseInt(nombre)) {
+    while (nombre === "" || parseInt(nombre) || !nombre){
         nombre = prompt("Su nombre no puede estar vacío ni ser un número - por favor repita:");
-    }
+    };
 };
 
 // Pide la dirección de inicio, repite mientras no se ingrese una
-function pideDireccionInicio(direccionInicio) {
+function pideDireccionInicio() {
     direccionInicio = prompt("Indique la dirección de inicio:");
-    while (direccionInicio === "" || parseInt(direccionInicio)) {
+    while (direccionInicio === "" || parseInt(direccionInicio) || !direccionInicio) {
         direccionInicio = prompt("La dirección de inicio no puede estar vacía ni ser sólo un número - por favor repita:");
-    }
+    };
 };
 
 // Pide la dirección de término, repite mientras no se ingrese una
-function pideDireccionTermino(direccionFin) {
+function pideDireccionTermino() {
     direccionFin = prompt("Indique la dirección de destino:");
-    while (direccionFin === "" || parseInt(direccionFin)) {
+    while (direccionFin === "" || parseInt(direccionFin) || !direccionFin) {
         direccionFin = prompt("La dirección de destino no puede estar vacía ni ser sólo un número - por favor repita:");
-    }
+    };
 };
 
 // Ya que aún no puedo calcular la distancia, le asigno un valor aleatorio
-function defineDistancia(kilometros) {
+function defineDistancia() {
     kilometros = Math.random() * (distanciaMaxima - distanciaMinima) + distanciaMinima;
     kilometros = parseFloat(kilometros.toFixed(1));
 };
 
 // Calcula un precio según el rango de kilómetros
-function calculaPrecio(kilometros) {
-        console.log(kilometros, precio);
+function calculaPrecio() {
+    defineDistancia();
     if (kilometros <= 30) {
         precio = kilometros * 2000;
     } else if (kilometros > 30 && kilometros <= 60) {
         precio = kilometros * 1500;
     } else {
         precio = kilometros * 1000;
-    }
-    console.log(kilometros, precio);
+    };
 }
 
 // Función de salida
-
+function muestraResultado() {
+    alert(nombre + ", un viaje desde " + direccionInicio + " hasta " + direccionFin + " son " + kilometros + " kilómetros, y te costará $" + precio);
+};
 
 
 /* -------------------------------------------------------------------------- */
 /*                               Llama funciones                              */
 /* -------------------------------------------------------------------------- */
-pideNombre(nombre);
-pideDireccionInicio(direccionInicio);
-pideDireccionTermino(direccionFin);
-defineDistancia(kilometros);
-calculaPrecio(kilometros);
+pideNombre();
+pideDireccionInicio();
+pideDireccionTermino();
+calculaPrecio();
+muestraResultado();
