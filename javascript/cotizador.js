3d4619bb6736afd3c07bc4f2eec2c9fc8fba7095
René Donaire <rene.donaire@gmail.com>
2021-06-19 17:15:56 -0400

    Separa datos pedidos en calle. numero y ciudad

--- javascript/cotizador.js
+++ javascript/cotizador.js
@@ -9,10 +9,9 @@ const distanciaMaxima = 100;
 /*                              Define variables                              */
 /* -------------------------------------------------------------------------- */
 let nombre = "";
-let direccionInicio = "";
-let direccionFin = "";
 let kilometros = 0;
 let precio = 0;
+let direcciones = [];
 
 /* -------------------------------------------------------------------------- */
 /*                               Define funciones                             */
@@ -25,19 +24,35 @@ function pideNombre() {
     };
 };
 
-// Pide la dirección de inicio, repite mientras no se ingrese una
+// Pide la dirección de inicio, repite mientras no se ingrescorrectamente
 function pideDireccionInicio() {
-    direccionInicio = prompt("Indique la dirección de inicio:");
-    while (direccionInicio === "" || parseInt(direccionInicio) || !direccionInicio) {
-        direccionInicio = prompt("La dirección de inicio no puede estar vacía ni ser sólo un número - por favor repita:");
+    calleInicio = prompt("Dirección de inicio: Indique la calle");
+    while (calleInicio === "" || parseInt(calleInicio) || !calleInicio) {
+        calleInicio = prompt("La calle no puede estar vacía ni ser sólo un número - por favor repita:");
+    };
+    numeroInicio = prompt("Dirección de inicio: Indique el número");
+    while (numeroInicio === "" || !parseInt(numeroInicio) || !numeroInicio) {
+        numeroInicio = prompt("El número no puede estar vacío ni contener letras - por favor repita:");
+    };
+    ciudadInicio = prompt("Dirección de inicio: Indique la ciudad");
+    while (ciudadInicio === "" || parseInt(ciudadInicio) || !ciudadInicio) {
+        ciudadInicio = prompt("La ciudad no puede estar vacía ni ser sólo un número - por favor repita:");
     };
 };
 
-// Pide la dirección de término, repite mientras no se ingrese una
+// Pide la dirección de término, repite mientras no se ingrese correctamente
 function pideDireccionTermino() {
-    direccionFin = prompt("Indique la dirección de destino:");
-    while (direccionFin === "" || parseInt(direccionFin) || !direccionFin) {
-        direccionFin = prompt("La dirección de destino no puede estar vacía ni ser sólo un número - por favor repita:");
+    calleTermino = prompt("Dirección de término: Indique la calle");
+    while (calleTermino === "" || parseInt(calleTermino) || !calleTermino) {
+        calleTermino = prompt("La calle no puede estar vacía ni ser sólo un número - por favor repita:");
+    };
+    numeroTermino = prompt("Dirección de término: Indique el número");
+    while (numeroTermino === "" || !parseInt(numeroTermino) || !numeroTermino) {
+        numeroTermino = prompt("El número no puede estar vacío ni contener letras - por favor repita:");
+    };
+    ciudadTermino = prompt("Dirección de término: Indique la ciudad");
+    while (ciudadTermino === "" || parseInt(ciudadTermino) || !ciudadTermino) {
+        ciudadTermino = prompt("La ciudad no puede estar vacía ni ser sólo un número - por favor repita:");
     };
 };
 
@@ -57,19 +72,46 @@ function calculaPrecio() {
     } else {
         precio = kilometros * 1000;
     };
+};
+
+//Clase Direccion, construye las direcciones como objetos
+class Direccion {
+    constructor(calle, numero, ciudad) {
+        this.calle = calle;
+        this.numero = numero;
+        this.ciudad = ciudad;
+    }
+};
+
+//Esta función guarda las direcciones en un array
+function guardaDirecciones (direccion) {
+    guardar = this.direccion;
+    direcciones.push(guardar);
 }
 
 // Función de salida
 function muestraResultado() {
-    alert(nombre + ", un viaje desde " + direccionInicio + " hasta " + direccionFin + " son " + kilometros + " kilómetros, y te costará $" + precio);
+    alert(nombre + ", un viaje desde " + direccionInicio.calle + " hasta " + direccionTermino.calle + " son " + kilometros + " kilómetros, y te costará $" + precio);
 };
 
 
 /* -------------------------------------------------------------------------- */
 /*                               Llama funciones                              */
 /* -------------------------------------------------------------------------- */
+//Pide los datos
 pideNombre();
 pideDireccionInicio();
 pideDireccionTermino();
+const direccionInicio = new Direccion(calleInicio, numeroInicio, ciudadInicio);
+const direccionTermino = new Direccion(calleTermino, numeroTermino, ciudadTermino);
+console.log(direccionInicio);
+console.log(direccionTermino);
+
+//Guarda direcciones en un array
+guardaDirecciones(direccionInicio);
+guardaDirecciones(direccionTermino);
+console.log(direcciones);
+
+//Calcula el precio y muestra el resultado
 calculaPrecio();
 muestraResultado();
