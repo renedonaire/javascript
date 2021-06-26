8fe5b0d27c3f2d577efef3f8662c45f16f66e41e
René Donaire <rene.donaire@gmail.com>
2021-06-26 01:44:20 -0400

    Agrega o quita el botón Cotizar mediante función

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -3,45 +3,75 @@
 /* -------------------------------------------------------------------------- */
 // Valida los campos del formulario al hacer click en el botón y muestra los hints corresponientes
 function valida() {
-        let servicio = document.getElementById("servicio").value;
+    // Array para validar las entradas con True o False
+    let validado = [];
+
+    // Valida que el tipo de servicio no esté vacío
+    let servicio = document.getElementById("servicio").value;
     if (servicio == ".") {
         document.getElementById("labelServicio").innerHTML = "Tipo de servicio requerido";
         document.getElementById("labelServicio").classList = "error";
+        validado.push("false");
     } else {
         document.getElementById("labelServicio").innerHTML = "";
+        validado.push("true");
     };
 
+    // Valida que el nombre no esté vacío o sean sólo números
     let nombre = document.getElementById("nombre").value;
-        if (nombre === "" || parseInt(nombre) || !nombre) {
+    if (nombre === "" || parseInt(nombre) || !nombre) {
         document.getElementById("labelNombre").innerHTML = "Nombre requerido, sin cifras";
         document.getElementById("labelNombre").classList = "error";
+        validado.push("false");
     } else {
         document.getElementById("labelNombre").innerHTML = "";
+        validado.push("true");
     };
 
+    // Valida que el teléfono contenga sólo números
     let telefono = document.getElementById("telefono").value;
-        if (!parseInt(telefono)) {
+    if (!parseInt(telefono)) {
         document.getElementById("labelTelefono").innerHTML = "Teléfono requerido, sólo cifras";
         document.getElementById("labelTelefono").classList = "error";
+        validado.push("false");
     } else {
         document.getElementById("labelTelefono").innerHTML = "";
+        validado.push("true");
     };
 
+    // Valida que haya una dirección de inicio
     let direccionInicio = document.getElementById("direccionInicio").value;
-        if (direccionInicio === "" || parseInt(direccionInicio) || !direccionInicio) {
+    if (direccionInicio === "" || parseInt(direccionInicio) || !direccionInicio) {
         document.getElementById("labelDireccionInicio").innerHTML = "Se requiere dirección de inicio";
         document.getElementById("labelDireccionInicio").classList = "error";
+        validado.push("false");
     } else {
         document.getElementById("labelDireccionInicio").innerHTML = "";
+        validado.push("true");
     };
 
+    // Valida que exista una dirección de destino
     let direccionTermino = document.getElementById("direccionTermino").value;
     if (direccionTermino === "" || parseInt(direccionTermino) || !direccionTermino) {
         document.getElementById("labelDireccionTermino").innerHTML = "Se requiere dirección de término";
         document.getElementById("labelDireccionTermino").classList = "error";
+        validado.push("false");
     } else {
         document.getElementById("labelDireccionTermino").innerHTML = "";
+        validado.push("true");
     };
+
+    // Si en el array de control todos los valores son True, agrega el botón al DOM
+    if (!validado.includes("false")) {
+        alert("Todo OK weasman");
+        let btn = document.createElement("button");
+        btn.innerHTML = "Cotizar";
+        document.getElementById("formulario").appendChild(btn);
+        btn.className += "boton";
+    } else {
+        let form = document.getElementById("formulario");
+        form.removeChild(form.childNodes[5]);
+    }
 };
 
 // Ya que aún no puedo calcular la distancia, le asigno un valor aleatorio
