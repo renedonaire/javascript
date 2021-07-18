7e6554cb2e844d6e6838778e0756fa48ff0d0761
René Donaire <rene.donaire@gmail.com>
2021-07-17 21:08:54 -0400

    Da movimiento al mapa

--- javascript/cotizador.js
+++ javascript/cotizador.js
@@ -10,3 +10,12 @@ const distanciaMaxima = 100;
 /* -------------------------------------------------------------------------- */
 let kilometros = 0; //distancia al azar -temporalmente
 let precio = 0; //precio calculado
+
+// No hay mucho para animar... todos los elementos están en grid :(
+$(document).ready(function(){
+    $("#map").animate({
+            right: -50,
+            top: -10,
+				},
+				1000, );
+        });
