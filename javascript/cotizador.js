9f0f659efcb282706e9aaf7f3c973e447795183f
René Donaire <rene.donaire@gmail.com>
2021-08-06 22:32:30 -0400

    Agrega objeto 'services'

--- javascript/cotizador.js
+++ javascript/cotizador.js
@@ -10,6 +10,15 @@ const distanciaMaxima = 100;
 /* -------------------------------------------------------------------------- */
 let kilometros = 0; //distancia al azar -temporalmente
 let precio = 0; //precio calculado
+let services = {
+  "services": [
+    { "tipoServicio": "Delivery", "precioPorKm": "100" },
+    { "tipoServicio": "Mudanza", "precioPorKm": "300" },
+    { "tipoServicio": "Camioneta Rampa", "precioPorKm": "600" }
+  ]
+};
+
+
 
 // No hay mucho para animar... todos los elementos están en grid :(
 $(document).ready(function(){
